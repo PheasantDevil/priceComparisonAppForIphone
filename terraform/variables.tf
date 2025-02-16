5a823e877676af5a5832990faf3fdfc4f8f469c7
KonishiKenji <konishi.b0engineer@gmail.com>
2025-02-16 17:56:39 +0900

    chore: update gitignore to exclude lambda dependencies

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "price-comparison"
+}
