f447e9c11c8dcf82088069e79521dac4cf470332
KonishiKenji <konishi.b0engineer@gmail.com>
2025-03-29 22:06:03 +0900

    ci: fix error_03292206

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,48 +1,52 @@
-resource "aws_api_gateway_rest_api" "price_comparison" {
+resource "aws_api_gateway_rest_api" "api" {
   name        = "price-comparison-api"
   description = "API for iPhone price comparison"
 }
 
 resource "aws_api_gateway_resource" "prices" {
-  rest_api_id = aws_api_gateway_rest_api.price_comparison.id
-  parent_id   = aws_api_gateway_rest_api.price_comparison.root_resource_id
-  path_part   = "get_prices"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "prices"
 }
 
-resource "aws_api_gateway_method" "get_prices" {
-  rest_api_id   = aws_api_gateway_rest_api.price_comparison.id
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.prices.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.price_comparison.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.prices.id
-  http_method = aws_api_gateway_method.get_prices.http_method
+  http_method = aws_api_gateway_method.get.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = try(data.aws_lambda_function.existing_get_prices[0].invoke_arn, aws_lambda_function.get_prices[0].invoke_arn)
+  uri                     = try(data.aws_lambda_function.existing_get_prices.invoke_arn, aws_lambda_function.get_prices[0].invoke_arn)
 }
 
-resource "aws_api_gateway_deployment" "price_comparison" {
-  rest_api_id = aws_api_gateway_rest_api.price_comparison.id
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
   depends_on  = [aws_api_gateway_integration.lambda]
 }
 
 resource "aws_api_gateway_stage" "prod" {
-  deployment_id = aws_api_gateway_deployment.price_comparison.id
-  rest_api_id   = aws_api_gateway_rest_api.price_comparison.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
   stage_name    = "prod"
 }
 
 resource "aws_lambda_permission" "api_gateway" {
-  count = try(data.aws_lambda_function.existing_get_prices[0].function_name, "") != "" ? 0 : 1
+  count = try(data.aws_lambda_function.existing_get_prices.function_name, "") != "" ? 0 : 1
 
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = try(data.aws_lambda_function.existing_get_prices[0].function_name, aws_lambda_function.get_prices[0].function_name)
+  function_name = try(data.aws_lambda_function.existing_get_prices.function_name, aws_lambda_function.get_prices[0].function_name)
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.price_comparison.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+output "api_gateway_url" {
+  value = "${aws_api_gateway_stage.prod.invoke_url}${aws_api_gateway_resource.prices.path}"
 }
