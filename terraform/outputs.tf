f59ea3d93c101e019cc5c9356358a8a0aa657b17
KonishiKenji <konishi.b0engineer@gmail.com>
2025-03-29 22:10:05 +0900

    ci: fix error_03292210

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "api_gateway_url" {
   description = "API Gateway endpoint URL"
-  value       = "${aws_api_gateway_stage.prod.invoke_url}/get_prices"
+  value       = "${aws_api_gateway_stage.prod.invoke_url}${aws_api_gateway_resource.prices.path}"
 }
