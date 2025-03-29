e03afa95718a0ccf9dad9381d143181a5fdb5781
KonishiKenji <konishi.b0engineer@gmail.com>
2025-03-29 13:17:02 +0900

    ci: fix error_03291317

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,8 +2,3 @@ output "api_gateway_url" {
   description = "API Gateway endpoint URL"
   value       = "${aws_api_gateway_stage.prod.invoke_url}/get_prices"
 }
-
-output "lambda_function_name" {
-  description = "Lambda function name"
-  value       = aws_lambda_function.get_prices.function_name
-}
