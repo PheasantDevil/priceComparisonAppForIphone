9b87129aa92d9e65c3faf29f220f31adadb5bc02
KonishiKenji <konishi.b0engineer@gmail.com>
2025-03-29 15:50:01 +0900

    ci: fix error_03291550

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -23,40 +23,30 @@ resource "aws_api_gateway_integration" "lambda" {
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = try(
-    data.aws_lambda_function.existing_get_prices.invoke_arn,
-    try(aws_lambda_function.get_prices[0].invoke_arn, "")
-  )
+  uri                     = try(data.aws_lambda_function.existing_get_prices[0].invoke_arn, aws_lambda_function.get_prices[0].invoke_arn)
 }
 
-resource "aws_api_gateway_deployment" "api" {
+resource "aws_api_gateway_deployment" "price_comparison" {
   rest_api_id = aws_api_gateway_rest_api.price_comparison.id
-
-  depends_on = [
-    aws_api_gateway_integration.lambda
-  ]
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  depends_on  = [aws_api_gateway_integration.lambda]
 }
 
 resource "aws_api_gateway_stage" "prod" {
-  deployment_id = aws_api_gateway_deployment.api.id
+  deployment_id = aws_api_gateway_deployment.price_comparison.id
   rest_api_id   = aws_api_gateway_rest_api.price_comparison.id
   stage_name    = "prod"
 }
 
-# Lambda関数のAPI Gateway呼び出し許可（存在しない場合のみ作成）
 resource "aws_lambda_permission" "api_gateway" {
-  count = try(data.aws_lambda_function.existing_get_prices.function_name, "") != "" ? 0 : 1
+  count = try(data.aws_lambda_function.existing_get_prices[0].function_name, "") != "" ? 0 : 1
 
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = try(
-    data.aws_lambda_function.existing_get_prices.function_name,
-    try(aws_lambda_function.get_prices[0].function_name, "")
-  )
+  function_name = try(data.aws_lambda_function.existing_get_prices[0].function_name, aws_lambda_function.get_prices[0].function_name)
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.price_comparison.execution_arn}/*/*"
+}
+
+output "api_gateway_url" {
+  value = "${aws_api_gateway_stage.prod.invoke_url}${aws_api_gateway_resource.prices.path}"
 }
