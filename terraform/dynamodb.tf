af42b759f9637c4d587b4cb666aad68a04aaa6cc
KonishiKenji <konishi.b0engineer@gmail.com>
2025-05-09 00:45:10 +0900

    chore: dynamoDBで不要なテーブルの削除

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,71 +1,6 @@
 # DynamoDBテーブルのデータ管理
-resource "aws_dynamodb_table_item" "iphone_prices_data" {
-  count = var.create_sample_data ? 1 : 0
-
-  table_name = aws_dynamodb_table.iphone_prices.name
-  hash_key   = "series"
-  range_key  = "capacity"
-
-  item = jsonencode({
-    series = {
-      S = "iPhone 16"
-    }
-    capacity = {
-      S = "128GB"
-    }
-    price = {
-      N = "999"
-    }
-    timestamp = {
-      S = timestamp()
-    }
-  })
-}
 
 # DynamoDBテーブルの定義
-resource "aws_dynamodb_table" "iphone_prices" {
-  name           = "iphone_prices"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "series"
-  range_key      = "capacity"
-
-  attribute {
-    name = "series"
-    type = "S"
-  }
-
-  attribute {
-    name = "capacity"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "CapacityIndex"
-    hash_key        = "series"
-    range_key       = "capacity"
-    projection_type = "ALL"
-  }
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  lifecycle {
-    ignore_changes = [
-      read_capacity,
-      write_capacity,
-      billing_mode
-    ]
-    # prevent_destroy = true  # 一時的に無効化
-  }
-
-  tags = {
-    Name        = "iphone_prices"
-    Environment = "production"
-    Project     = "iphone_price_tracker"
-  }
-}
-
 resource "aws_dynamodb_table" "official_prices" {
   name           = "official_prices"
   billing_mode   = "PAY_PER_REQUEST"
@@ -103,36 +38,6 @@ resource "aws_dynamodb_table" "official_prices" {
 
   tags = {
     Name        = "official_prices"
-    Environment = "production"
-    Project     = "iphone_price_tracker"
-  }
-}
-
-# 価格情報を格納するメインテーブル
-resource "aws_dynamodb_table" "price_comparison" {
-  name           = "price-comparison"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
-  stream_enabled = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-  server_side_encryption {
-    enabled = true
-    kms_key_arn = aws_kms_key.data_encryption.arn
-  }
-
-  replica {
-    region_name = "ap-southeast-1"
-    kms_key_arn = aws_kms_key.data_encryption_replica.arn
-  }
-
-  tags = {
-    Name        = "price-comparison"
     Environment = "production"
     Project     = "iphone_price_tracker"
   }
